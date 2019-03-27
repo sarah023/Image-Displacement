1e03eb8e3e8f70c59cf256e23f9a829b855d29e6
Sarah Grundy <sarahgrundy023@gmail.com>
2019-03-27 16:03:35 +1030

    displacement filters amended - movement and wrapping added

--- dist/js/app.js
+++ dist/js/app.js
@@ -24,15 +24,24 @@ sections.forEach(section => {
   //add PIXI application to the section tags:
   section.appendChild(app.view);
 
+  //make a new image/filters here:
+  let image = null;
+  let displacementImage = null;
+
   //make a new loader:
   const loader = new PIXI.loaders.Loader();
 
   //load in image:
   loader.add('image', originalImageSource);
+  //load in displacement image: (for displacement filter)
+  //displacementImage sizes need to be either 512px x 512px or 1024px x 1024px
+  loader.add('displacement', './images/displacement3.jpg');
+
   //once the image has loaded -> do things:
   loader.load((loader, resources) => {
     //add the original image back as a 'texture':
-    const image = new PIXI.Sprite(resources.image.texture);
+    image = new PIXI.Sprite(resources.image.texture);
+    displacementImage = new PIXI.Sprite(resources.displacement.texture);
     //set the position and size of the image:
 
     //set position - size + the middle of the respective width/height
@@ -43,24 +52,40 @@ sections.forEach(section => {
     //correct image sizes.
     image.width = 400;
     image.height = 600;
+    image.interactive = true;
     //for certain transitions, need to set the anchor point of
     //the image to the center.
     //based on 0 - 1, so 0.5 is the middle/center point
     image.anchor.x = 0.5;
     image.anchor.y = 0.5;
 
+    //displacement image sizing (optional)
+    displacementImage.width = 300;
+    displacementImage.height = 300;
+    displacementImage.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.repeat;
+
     //*ADD FILTERS*
+    image.filters = [
+      // new PIXI.filters.BlurFilter(2, 5),
+      // new PIXI.filters.NoiseFilter(0.2),
+      new PIXI.filters.DisplacementFilter(displacementImage, 20)
+    ];
+
     //http://pixijs.download/release/docs/PIXI.filters.BlurFilter.html
-    image.filters = [new PIXI.filters.BlurFilter(2, 5)];
     //http://pixijs.download/release/docs/PIXI.filters.NoiseFilter.html
-    image.filters = [new PIXI.filters.NoiseFilter(0.2)];
+    //http://pixijs.download/release/docs/PIXI.filters.DisplacementFilter.html
 
-    //add the image to the app:
+    //*ADD IMAGES/FILTERS TO PAGE*
     app.stage.addChild(image);
+    app.stage.addChild(displacementImage);
 
-    // //*ROTATION VARIATION*
-    // app.ticker.add(() => {
-    //   image.rotation = image.rotation + 0.01;
-    // });
+    //*ADD MOVEMENT/TIMING*
+    app.ticker.add(() => {
+      //*rotation variation*
+      // image.rotation = image.rotation + 0.01;
+      //*displacementImage movement
+      displacementImage.x = displacementImage.x + 1;
+      displacementImage.y = displacementImage.y + 1;
+    });
   });
 });
